df520e13870c8e3c25805a5c5b56c1f9e9143313
Ali-Naeemi <aboll5303@gmail.com>
2025-11-03 18:15:22 +0330

    commit 1

---
+++ age_01.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name : ")
+
+b_year = int(input("Please enter your year of birth : "))
+
+c_year = 1404
+age = c_year - b_year
+
+print(f"Hello {name}, You are {age} years old.")
